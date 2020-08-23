9816436c27dcb36a84fd3364108697925ca11d7b
Aleksey Spivak <aspivak@gmail.com>
2020-08-23 14:00:42 -0500

    added accessors for PPU registers

--- NES/PPU.cpp
+++ NES/PPU.cpp
@@ -1,1 +1,11 @@
 #include "PPU.h"
+
+void PPU::PowerUp()
+{
+	// https://wiki.nesdev.com/w/index.php/PPU_power_up_state
+}
+
+void PPU::Reset()
+{
+	// https://wiki.nesdev.com/w/index.php/PPU_power_up_state
+}
--- NES/PPU.h
+++ NES/PPU.h
@@ -1,50 +1,145 @@
 #pragma once
 #include <cstdint>
+
+union PPUControlRegister
+{
+	struct Bits {
+		uint8_t m_base_nametable_addr : 2;
+		uint8_t m_vram_add : 1;
+		uint8_t m_sprite_pattern_addr : 1;
+		uint8_t m_bkgrnd_pattern_addr : 1;
+		uint8_t m_sprite_size : 1;
+		uint8_t m_ppu_master_slave : 1;
+		uint8_t m_generate_nmi : 1;
+
+	} Bits;
+	uint8_t Register = 0x00;
+};
+
+union PPUMaskRegister
+{
+	struct Bits {
+		uint8_t m_grayscale : 1;
+		uint8_t m_show_bkgrnd_leftmost : 1;
+		uint8_t m_show_sprites_leftmost : 1;
+		uint8_t m_show_bkgrnd : 1;
+		uint8_t m_show_sprites : 1;
+		uint8_t m_emphasize_red : 1;
+		uint8_t m_emphasize_green : 1;
+		uint8_t m_emphasize_blue : 1;
+
+	} Bits;
+	uint8_t Register = 0x00;
+};
+
+union PPUStatusRegister
+{
+	struct Bits {
+		uint8_t m_lsb_previous_write : 5;
+		uint8_t m_sprite_overflow : 1;
+		uint8_t m_sprite_zero_hit : 1;
+		uint8_t m_vertical_blank_started : 1;
+
+	} Bits;
+	uint8_t Register = 0x00;
+};
+
 class PPU
 {
-	union PPUControlRegister
+public:
+	void PowerUp();
+	void Reset();
+
+	uint8_t GetOAMDMA()
 	{
-		struct Bits {
-			uint8_t m_base_nametable_addr : 2;
-			uint8_t m_vram_add : 1;
-			uint8_t m_sprite_pattern_addr : 1;
-			uint8_t m_bkgrnd_pattern_addr : 1;
-			uint8_t m_sprite_size : 1;
-			uint8_t m_ppu_master_slave : 1;
-			uint8_t m_generate_nmi : 1;
+		return m_oam_dma;
+	}
 
-		} Bits;
-		uint8_t Register = 0x00;
-	};
-
-	union PPUMaskRegister
+	void SetOAMDMA(const uint8_t value)
 	{
-		struct Bits {
-			uint8_t m_grayscale : 1;
-			uint8_t m_show_bkgrnd_leftmost : 1;
-			uint8_t m_show_sprites_leftmost : 1;
-			uint8_t m_show_bkgrnd : 1;
-			uint8_t m_show_sprites : 1;
-			uint8_t m_emphasize_red : 1;
-			uint8_t m_emphasize_green : 1;
-			uint8_t m_emphasize_blue : 1;
+		m_oam_dma = value;
+	}
 
-		} Bits;
-		uint8_t Register = 0x00;
-	};
-
-	union PPUStatusRegister
+	uint8_t GetData()
 	{
-		struct Bits {
-			uint8_t m_lsb_previous_write : 5;
-			uint8_t m_sprite_overflow : 1;
-			uint8_t m_sprite_zero_hit : 1;
-			uint8_t m_vertical_blank_started : 1;
+		return m_ppu_data;
+	}
 
-		} Bits;
-		uint8_t Register = 0x00;
-	};
+	void SetData(const uint8_t value)
+	{
+		m_ppu_data = value;
+	}
 
+	uint8_t GetAddress()
+	{
+		return m_ppu_addr;
+	}
+
+	void SetAddress(const uint8_t value)
+	{
+		m_ppu_addr = value;
+	}
+
+	uint8_t GetScroll()
+	{
+		return m_ppu_scroll;
+	}
+
+	void SetScroll(const uint8_t value)
+	{
+		m_ppu_scroll = value;
+	}
+
+	uint8_t GetOAMData()
+	{
+		return m_reg_oam_data;
+	}
+
+	void SetOAMData(const uint8_t value)
+	{
+		m_reg_oam_data = value;
+	}
+	
+	uint8_t GetOAMAddress()
+	{
+		return m_reg_oam_addr;
+	}
+
+	void SetOAMAddress(const uint8_t value)
+	{
+		m_reg_oam_addr = value;
+	}
+
+	PPUStatusRegister GetStatusRegister()
+	{
+		return m_reg_status;
+	}
+
+	void SetStatusRegister(const uint8_t value)
+	{
+		m_reg_status.Register = value;
+	}
+
+	PPUMaskRegister GetMaskRegister()
+	{
+		return m_reg_ppu_mask;
+	}
+
+	void SetMaskRegister(const uint8_t value)
+	{
+		m_reg_ppu_mask.Register = value;
+	}
+	
+	PPUControlRegister& GetControlRegister()
+	{
+		return m_reg_ppu_control;
+	}
+
+	void SetControlRegister(const uint8_t value)
+	{
+		m_reg_ppu_control.Register = value;
+	}
+private:
 	PPUControlRegister m_reg_ppu_control;
 	PPUMaskRegister m_reg_ppu_mask;
 	PPUStatusRegister m_reg_status;
