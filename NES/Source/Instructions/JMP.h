300fc35b75034789d44cb167c103e47d495ca821
Aleksey Spivak <aspivak@gmail.com>
2021-05-01 18:50:41 -0500

    jmp++

--- NES/Source/Instructions/JMP.h
+++ NES/Source/Instructions/JMP.h
@@ -1,57 +1,55 @@
-#ifndef NES_JMP_H
-#define NES_JMP_H
+#pragma once
 
-#include <iomanip>
-#include "../Addressing/AddressingMode.h"
 #include "BaseInstruction.h"
 
-
-
-class JMPAbsolute : public BaseInstruction<JMPAbsoluteAddressingStrategy, JMPAbsolute, 0x4C>
+// http://www.obelisk.me.uk/6502/reference.html#JMP
+template<class _addressing_strategy>
+class JMPAbsoluteBase : public OpCodeBase<_addressing_strategy>
 {
 public:
-	JMPAbsolute() : BaseInstruction(3, "JMP")
+	JMPAbsoluteBase() : OpCodeBase<_addressing_strategy>("JMP")
 	{
-		m_increments_program_counter = false;
+		this->m_increments_program_counter = false;
 	}
 
 	void ExecuteImplementation()
 	{
 		CPU& cpu = NESConsole::GetInstance()->GetCPU();
-		Memory& memory = NESConsole::GetInstance()->GetMemory();
-		cpu.SetRegisterProgramCounter(GetAddressingMode().GetMemoryShort());
+		cpu.SetRegisterProgramCounter(this->GetAddressingMode().GetMemoryShort());
 	}
 };
 
-class JMPIndirect : public BaseInstruction<IndirectAddressingStrategy, JMPIndirect, 0x6C>
+template<class _addressing_strategy>
+class JMPIndirectBase : public OpCodeBase<_addressing_strategy>
 {
 public:
-	JMPIndirect() : BaseInstruction(5, "JMP")
+	JMPIndirectBase() : OpCodeBase<_addressing_strategy>("JMP")
 	{
-		m_increments_program_counter = false;
+		this->m_increments_program_counter = false;
 	}
 
 	void ExecuteImplementation()
 	{
 		CPU& cpu = NESConsole::GetInstance()->GetCPU();
-		Memory& memory = NESConsole::GetInstance()->GetMemory();
-		cpu.SetRegisterProgramCounter(GetAddressingMode().GetMemoryShortValue());
+		cpu.SetRegisterProgramCounter(this->GetAddressingMode().GetMemoryShortValue());
 	}
 };
 
-class JSR : public BaseInstruction<JMPAbsoluteAddressingStrategy, JSR, 0x20>
+// http://www.obelisk.me.uk/6502/reference.html#JSR
+template<class _addressing_strategy>
+class JSRBase : public OpCodeBase<_addressing_strategy>
 {
 public:
-	JSR() : BaseInstruction(6, "JSR")
+	JSRBase() : OpCodeBase<_addressing_strategy>("JSR")
 	{
-		m_increments_program_counter = false;
+		this->m_increments_program_counter = false;
 	}
 
 	void ExecuteImplementation()
 	{
 		CPU& cpu = NESConsole::GetInstance()->GetCPU();
 		Memory& memory = NESConsole::GetInstance()->GetMemory();
-		uint16_t jump_address = GetAddressingMode().GetMemoryShort();
+		uint16_t jump_address = this->GetAddressingMode().GetMemoryShort();
 		uint16_t return_memory_address = cpu.GetRegisterProgramCounterPlus(3 - 1);
 		uint8_t return_memory_high = static_cast<uint8_t>((return_memory_address & 0xFF00) >> 8);
 		uint8_t return_memory_low = static_cast<uint8_t>(return_memory_address & 0x00FF);
@@ -63,4 +61,6 @@ public:
 	}
 };
 
-#endif //NES_JMP_H
+typedef BaseInstruction2<JMPAbsoluteBase<JMPAbsoluteAddressingStrategy>, 0x4C, 3> JMPAbsolute;
+typedef BaseInstruction2<JMPIndirectBase<IndirectAddressingStrategy>, 0x6C, 5> JMPIndirect;
+typedef BaseInstruction2<JSRBase<JMPAbsoluteAddressingStrategy>, 0x20, 6> JSR;
